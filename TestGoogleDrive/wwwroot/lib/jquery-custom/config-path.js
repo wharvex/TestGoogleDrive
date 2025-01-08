7d389cee36ffc9b315036afbae1663bac13dd6d1
Tim <81175852+wharvex@users.noreply.github.com>
2025-01-08 16:16:48 -0500

    config path lookup refactor
    Refactor: Move config-path-lookup jQuery script to its own file.

---
+++ TestGoogleDrive/wwwroot/lib/jquery-custom/config-path.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () {
+    $("#configSelect").change(function () {
+        var selectedConfigId = this.value;
+        $.ajax({
+            url: '/TestRuns/GetConfigPath',
+            data: { configId: selectedConfigId },
+            success: function (data) {
+                console.log(data.configPath);
+            }
+        });
+    });
+});
